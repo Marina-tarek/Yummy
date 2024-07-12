1db36b75e7f6d8248e4e507ed8241298d0d36040
Marina-tarek <marina.tarek.w@gmail.com>
2024-07-12 17:29:40 +0300

    Sixth commit

--- js/index.js
+++ js/index.js
@@ -39,7 +39,7 @@ function displayCategory(array) {
     for (let i = 0; i < array.length; i++) {
         cartona += `
 <div class="col-md-3">
-                <div onclick="filterCategoryMeal('${array[i].strCategory}')" class="pointer position-relative category-img">
+                <div onclick="filterCategoryMeal('${array[i].strCategory}')" class="pointer position-relative category-img rounded-2">
                     <img class="w-100" src='${array[i].strCategoryThumb}' alt="${array[i].strCategory}" srcset="">
                     <div class="OverLayerMeal  position-absolute text-black text-center">
                         <h3 class="fw-medium fs-3 text-center" id="category">${array[i].strCategory}</h3>
@@ -56,15 +56,15 @@ function displayCategory(array) {
 async function filterCategoryMeal(category) {
     let response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
     data = await response.json()
-    // console.log(data.meals);
     displayMealCategory(data.meals)
 }
+///-----control--{Main Fnction}--------> displayMealCategory/&/displayMealsOfThisArea /&/displayMealsIngredient
 function displayMealCategory(array) {
     let cartona = ``
     for (let i = 0; i < array.length; i++) {
         cartona += `
     <div class="col-md-3">
-                    <div onclick="getMealInstructions('${array[i].idMeal}')" class="pointer position-relative category-img">
+                    <div onclick="getMealInstructions('${array[i].idMeal}')" class="pointer position-relative category-img rounded-2">
                         <img class="w-100" src='${array[i].strMealThumb}' alt="${array[i].strMeal}">
                         <div class="OverLayerMeal  position-absolute text-black text-center d-flex align-items-center justify-content-center">
                             <h3 class="fw-medium fs-3 text-center" id="category">${array[i].strMeal}</h3>
@@ -79,7 +79,7 @@ function displayMealCategory(array) {
 async function getMealInstructions(idMeal) {
     let response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
     data = await response.json()
-    console.log(data.meals[0]);
+    // console.log(data.meals[0]);
     displayMealInstructions(data.meals[0])
 }
 function displayMealInstructions(food) {
@@ -109,8 +109,8 @@ recepies +=` <i class="alert alert-info m-2 p-1">${food[`strMeasure${i}`]}${food
 <div class="col-md-8">
     <h2>instructions</h2>
     <p>${food.strInstructions}</p>
-    <h3>Area : <span class="fw-light">${food.strArea}</span> </h3>
-    <h3>Category : <span class="fw-light"> ${food.strCategory}</span> </h3>
+    <h3><span class="fw-bolder">Area : </span>${food.strArea} </h3>
+    <h3><span class="fw-bolder">Category :</span> ${food.strCategory}</h3>
     <h3>recipes :</h3>
     <ul class="list-unstyled recipes-list d-flex flex-wrap">
  ${recepies}
@@ -125,6 +125,73 @@ recepies +=` <i class="alert alert-info m-2 p-1">${food[`strMeasure${i}`]}${food
         rowData.innerHTML = cartoona
 
   
+}
+
+// --->Area Section
+$("#area").on("click",function(){
+    getArea()
+})
+async function getArea(){
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?a=list`);
+    area = await response.json();
+    displayArea(area.meals)
+}
+
+function displayArea(area){
+    let cartona = ``
+    for (let i = 0; i < area.length; i++) {
+        cartona += `
+<div class="col-md-3">
+                <div onclick="getMealsOfThisArea('${area[i].strArea}')" class="pointer position-relative category-img rounded-2">
+                  <i class="fa-solid fa-house-laptop area-icon"></i>
+                <h3 class="fw-medium fs-3 text-center" id="category">${area[i].strArea}</h3>
+                    </div>
+                </div>
+        </div>
+        `
+        rowData.innerHTML = cartona
+    }
+}
+async function getMealsOfThisArea(local){
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${local}`);
+    areaMeals = await response.json();
+    displayMealCategory(areaMeals.meals);
+}
+
+
+// --->ingrediant section
+$("#ingredient").on("click",function(){
+    getIngrediant()
+})
+async function getIngrediant(){
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?i=list`)
+    ingrediantData = await response.json()
+    displayIngredient(ingrediantData.meals.slice(0, 20))
+    console.log(ingrediantData.meals.slice(0, 20));
+}
+
+function displayIngredient(gredient){
+    let cartona = ``
+    for (let i = 0; i < gredient.length; i++) {
+        cartona += `
+<div class="col-md-3">
+                <div onclick="getMealsIngredient('${gredient[i].strIngredient}')" class="pointer position-relative category-img rounded-2 text-center">
+                 <i class="fa-solid fa-drumstick-bite area-icon"></i>
+                <h3 class="fw-medium fs-3 text-center" id="gred">${gredient[i].strIngredient}</h3>
+                 <p class="short-paragraph" >${gredient[i].strDescription.split(" ").slice(0, 20).join(" ")}</p>
+                    </div>
+                </div>
+        </div>
+        `
+        rowData.innerHTML = cartona
+    }
+    
+}
+
+async function getMealsIngredient(component){
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${component}`)
+   let MealsIngredient = await response.json()
+    displayMealCategory(MealsIngredient.meals)
 }
 
 
